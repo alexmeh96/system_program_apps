d00898a352e15c151178d2fe164d3bc4b8b19472
Alexandr Meshkov <alex.meh96@mail.ru>
2024-03-10 22:59:10 +0300

    rust_tcp_impl: edit

--- rust_tcp_impl/run.sh
+++ rust_tcp_impl/run.sh
@@ -3,7 +3,7 @@ cargo b --release
 sudo setcap 'cap_net_admin=eip' target/release/rust_tcp_impl
 ./target/release/rust_tcp_impl &
 pid=$!
-sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip addr add 10.0.0.1/24 dev tun0
 sudo ip link set up dev tun0
 trap 'kill $pid' INT TERM
 wait $pid
--- rust_tcp_impl/src/main.rs
+++ rust_tcp_impl/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+
 use tun_tap::Mode::Tun;
 
 fn main() -> io::Result<()> {
@@ -6,15 +7,29 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let n_bytes = nic.recv(&mut buf[..]).unwrap();
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        eprintln!(
-            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
-            n_bytes - 4,
-            flags,
-            proto,
-            &buf[4..n_bytes]
-        );
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // no ipv4
+            continue;
+        }
+        
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..n_bytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                eprintln!(
+                    "{} -> {} {:?}b of protocol {:x?}",
+                    src,
+                    dst,
+                    p.payload_len().unwrap(),
+                    proto,
+                );
+            }
+            Err(e) => {
+                eprintln!("ignoring weird packet {}", e)
+            }
+        }
     }
-    Ok(())
 }
