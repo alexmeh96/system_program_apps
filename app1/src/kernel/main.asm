d6bb1f1b7cc52a790eef1350a9397bc7da6d7824
Alexandr Meshkov <alex.meh96@mail.ru>
2024-03-26 14:05:57 +0300

    app1: asm and c together

--- app1/src/kernel/main.asm
+++ app1/src/kernel/main.asm
@@ -1,34 +1,20 @@
-ORG 0x0
 BITS 16
 
-start:
-    mov si,os_boot_msg
-    call print
+section _ENTRY CLASS=CODE
+
+extern _cstart_
+
+global entry
+
+entry:
+    cli
+    mov ax, ds
+    mov ss, ax
+    mov sp, 0
+    mov bp, sp
+    sti
+
+    call _cstart_
+
+    cli
     hlt
-
-halt:
-    jmp halt
-
-print:
-    push si
-    push ax
-    push bx
-
-print_loop:
-    lodsb
-    or al,al
-    jz done_print
-
-    mov ah, 0x0E
-    mov bh, 0
-    int 0x10
-
-    jmp print_loop
-
-done_print:
-    pop bx
-    pop ax
-    pop si
-    ret
-
-os_boot_msg: db 'Our OS has booted!!!', 0x0D, 0x0A, 0
